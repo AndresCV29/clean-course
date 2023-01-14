f3d837ae68d3873c086cd107ca5b37696df79b8c
AndresCV29 <adcv980318@gmail.com>
2023-01-14 13:59:18 -0500

    Aplicando DRY

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -5,14 +5,28 @@ class Product {
         public name: string = '',
         public price: number = 0,
         public size: Size = '',
-    ) {  }
+    ) {}
+
+    isProductReady(): boolean {
+        for(const key in this) {
+            console.log('here! ' + this[key])
+            switch(typeof this[key]) {
+                case 'string':
+                    if(String(this[key]).length <= 0) throw Error (`${key} is empty`)
+                break;
+                case 'number':
+                    if(String(this[key]).length <= 0) throw Error (`${key} is empty`)
+                break;
+                default:
+                    throw Error(`${typeof this[key]} is not valid`);
+            }
+        }
+        return true;
+    }
 
     toString() {
-        //No DRY
-        if ( this.name.length <= 0) throw Error('name is empty');
-        if ( this.price <= 0) throw Error('price is empty');
-        if ( this.size.length <= 0) throw Error('size is empty');
-
+        //DRY
+        if(!this.isProductReady) return
         return `${ this.name } ${this.price}, ${this.size}`
     }
 }
