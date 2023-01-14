0966257aa274f6ddd4e2ecfdd23900d493c6d627
AndresCV29 <adcv980318@gmail.com>
2023-01-14 15:51:39 -0500

    Forma corta de escribir clases - typescript

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,19 +1,12 @@
 (() =>{
     type Gender = 'M' | 'F';
     class Person {
-        public name: string;
-        public gender: string;
-        public birthdate: Date;
 
         constructor(
-            name: string,
-            gender: string,
-            birthdate: Date
-        ){
-            this.name = name;
-            this.gender = gender;
-            this.birthdate = birthdate;
-        }
+            public name: string,
+            public gender: string,
+            public birthdate: Date
+        ){}
 
     }
     const newPerson = new Person('Fernado', 'M', new Date (1895-10-21));
