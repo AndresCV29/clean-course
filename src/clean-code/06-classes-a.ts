54e6c5bab1c65e610c5d93639553c16e5756f1e1
AndresCV29 <adcv980318@gmail.com>
2023-01-15 13:23:27 -0500

    Herencia - problematica: No aplicacndo el principio de responsabilidad unica

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -9,6 +9,49 @@
         ){}
 
     }
-    const newPerson = new Person('Fernado', 'M', new Date (1895-10-21));
-    console.log({newPerson});
+
+    class User extends Person {
+        public lastAccess: Date
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(name, gender, birthdate)
+            this.lastAccess = new Date();
+        }
+        
+        checkCredentials () {
+            return true;
+        }
+    }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lasOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ){
+            super(email, role, name, gender, birthdate);
+        }
+    }
+
+    const userSettings = new UserSettings(
+        '/user/home',
+        '/home',
+        'fernando@google.com',
+        'Admin',
+        'Fernando',
+        'M',
+        new Date('1985-10-21')
+    );
+
+    console.log({userSettings});
+
 })()
