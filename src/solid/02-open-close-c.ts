ba7c7b3053e5388a409eae1de79969e80e08937c
AndresCV29 <adcv980318@gmail.com>
2023-01-21 11:45:54 -0500

    Removiendo la dependencia axios. No se afectan las demas clases

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,11 +1,21 @@
-import axios from 'axios';
+//import axios from 'axios';
+
+/*Esto se conoce como patron adaptador. Se crea una clase adaptadora para
+utilizar una libreria de terceros.*/
 
 export class HttpClient {
 
-    async get(url: string){
+    /* async get(url: string){
         const {data, status} = await axios.get(url);
         console.log({status});
         return {data, status};
+    } */
+
+    async get(url:string){
+        const resp = await fetch(url);
+        const data = await resp.json();
+
+        return {data: data, status: resp.status};
     }
 
 }
